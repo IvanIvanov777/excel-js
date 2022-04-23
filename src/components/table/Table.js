7b6aa21406698a78e04ab958e82ed3a2d693c29d
IvanIvanov777 <ii4453954@gmail.com>
2022-04-23 22:23:59 +0300

    Finish resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,43 +1,23 @@
+// import { $ } from '../../core/dom';
 import { ExcelComponent } from '../../core/ExcelComponent';
+import { shouldResize } from './table.functions';
+import { resizeHandler } from './table.resize';
 import { createTable } from './table.template';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown']
+    })
+  }
+
   toHTML = () => createTable(25)
 
-  // toHTML = () => `
-  // <div class="row">
-  //   <div class="row-info">
-
-  //   </div>
-  //   <div class="row-data">
-  //     <div class="column">A</div>
-  //     <div class="column">B</div>
-  //     <div class="column">C</div>
-  //   </div>
-  // </div>
-
-  // <div class="row">
-  //   <div class="row-info">
-  //     1
-  //   </div>
-  //   <div class="row-data">
-  //     <div class="cell selected" contenteditable>1</div>
-  //     <div class="cell" contenteditable>2</div>
-  //     <div class="cell" contenteditable>3</div>
-  //   </div>
-  // </div>
-
-  // <div class="row">
-  //   <div class="row-info">
-  //     2
-  //   </div>
-  //   <div class="row-data">
-  //     <div class="cell" contenteditable>1</div>
-  //     <div class="cell" contenteditable>2</div>
-  //     <div class="cell" contenteditable>3</div>
-  //   </div>
-  // </div>
-  // `
+  onMousedown(event) {
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event)
+    }
+  }
 }
